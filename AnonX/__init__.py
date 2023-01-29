678a99771370d2ac214d5e3594349d42456eed5d
Mix <111663064+MixDiv@users.noreply.github.com>
2023-01-29 22:36:48 +0200

    Update __init__.py

--- AnonX/__init__.py
+++ AnonX/__init__.py
@@ -1,6 +1,5 @@
 from AnonX.core.bot import AnonXBot
 from AnonX.core.dir import dirr
-from AnonX.core.git import git
 from AnonX.core.userbot import Userbot
 from AnonX.misc import dbb, heroku, sudo
 from aiohttp import ClientSession
@@ -10,7 +9,6 @@ from .logging import LOGGER
 
 dirr()
 
-git()
 
 dbb()
 
