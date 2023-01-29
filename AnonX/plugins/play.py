7cbd24d24bd4129904bde3d991a16a6cead34731
Mix <111663064+MixDiv@users.noreply.github.com>
2023-01-30 01:25:22 +0200

    Update play.py

--- AnonX/plugins/play.py
+++ AnonX/plugins/play.py
@@ -7,6 +7,7 @@ from pyrogram.types import (InlineKeyboardMarkup, InlineKeyboardButton, InputMed
                             Message)
 from pytgcalls.exceptions import NoActiveGroupCall
 
+
 import config
 from config import BANNED_USERS, lyrical
 from strings import get_command
