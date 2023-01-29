654dcdbdea056f8e58e2d3579e01be3c03855218
Mix <111663064+MixDiv@users.noreply.github.com>
2023-01-29 19:41:59 +0200

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -49,7 +49,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="- Dev Source .", url=config."P_T_I" 
+                text="- Dev Source .", url=config.UPSTREAM_REPO 
             )
         ],
      ]
