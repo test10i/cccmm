5007e8f5d53aced10d41c2e4a8857b3ab88ceae9
MIIIIX <125625184+MIIIIX@users.noreply.github.com>
2023-03-07 05:26:23 +0200

    Create id.py

---
+++ AnonX/plugins/id.py
@@ -0,0 +1,57 @@
+import re
+from pyrogram import Client, filters
+from pyrogram.enums import ChatMemberStatus
+
+PyroHub = Client("PyroHub - Pyrogram",
+api_id=, 
+api_hash="", 
+bot_token="")
+
+#Commands in *Tag .
+@PyroHub.on_message(filters.regex("^حظر .*$") & filters.group)
+async def ban(_, message):
+    User = re.match("حظر @(.*)", message.text).group(1)
+    stas = (await PyroHub.get_chat_member(message.chat.id, message.from_user.id)).status
+    auth = [ChatMemberStatus.OWNER, ChatMemberStatus.OWNER]
+    if stas in auth:
+    	await message.chat.ban_member(User)
+    	await message.reply_text("- تم حظره من المجموعة .")
+    else:
+            await message.reply("- هذا الأمر يخص مشرفين المجموعة .")
+            
+@PyroHub.on_message(filters.regex("^الغاء حظر .*$") & filters.group)
+async def unban(_, message):
+    User = re.match("الغاء حظر @(.*)", message.text).group(1)
+    stas = (await PyroHub.get_chat_member(message.chat.id, message.from_user.id)).status
+    auth = [ChatMemberStatus.OWNER, ChatMemberStatus.OWNER]
+    if stas in auth:
+    	await message.chat.unban_member(User)
+    	await message.reply_text("- تم الغاء حظره من المجموعة .")
+    else:
+            await message.reply("- هذا الأمر يخص مشرفين المجموعة .")
+            
+#Commands in *reply .
+@PyroHub.on_message(filters.regex("^حظر$") & filters.group)
+async def ban2(_, message):
+    user =message.reply_to_message.from_user.id
+    stas = (await PyroHub.get_chat_member(message.chat.id, message.from_user.id)).status
+    auth = [ChatMemberStatus.OWNER, ChatMemberStatus.OWNER]
+    if stas in auth:
+    	await PyroHub.ban_chat_member(message.chat.id,user)
+    	await message.reply_text("- تم حظره من المجموعة .")
+    else:
+            await message.reply("- هذا الأمر يخص مشرفين المجموعة .")
+            
+@PyroHub.on_message(filters.regex("^الغاء حظر$") & filters.group)
+async def ban3(_, message):
+    user =message.reply_to_message.from_user.id
+    stas = (await PyroHub.get_chat_member(message.chat.id, message.from_user.id)).status
+    auth = [ChatMemberStatus.OWNER, ChatMemberStatus.OWNER]
+    if stas in auth:
+    	await PyroHub.unban_chat_member(message.chat.id,user)
+    	await message.reply_text("- تم الغاء حظره من المجموعة .")
+    else:
+            await message.reply("- هذا الأمر يخص مشرفين المجموعة .")
+            
+print ("- Done Upload Telegram Bot .\n- Can You Used Bot Now!")
+PyroHub.run()
