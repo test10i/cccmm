eaec451541edc1e18f4c1a3e9dabbcea494fd188
Mmiiiimm <129563335+Mmiiiimm@users.noreply.github.com>
2023-04-06 02:50:24 +0200

    Create start.py

---
+++ AnonX/utils/inline/start.py
@@ -0,0 +1,56 @@
+from typing import Union
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+import config
+
+
+def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=". aDD Me To Your Groups .",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="ʜᴇʟᴩ",
+                callback_data="settings_back_helper",
+            ),
+            InlineKeyboardButton(
+                text="sᴇᴛᴛɪɴɢs", callback_data="settings_helper"
+            ),
+        ],
+     ]
+    return buttons
+
+
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=". aDD Me To Your Groups . ",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="- Help .", callback_data="settings_back_helper"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="- SUPPORT .", url=config.SUPPORT_GROUP
+            ),
+            InlineKeyboardButton(
+                text="- BoT OWNER .", user_id=OWNER
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="- Dev Source .", url=config.UPSTREAM_REPO
+            )
+        ],
+     ]
+    return buttons
